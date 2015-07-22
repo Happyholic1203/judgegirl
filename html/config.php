2664659dfbcb527332ae601e239b385350668e63
Yu-Cheng (Henry) Huang <Happyholic1203@gmail.com>
2015-07-22 13:38:53 +0800

    Packed into a re-distributable version.

--- html/config.php
+++ html/config.php
@@ -11,32 +11,32 @@ $StrCourseName = $StrCourse." 線上批改系統";
 $StrCourseNameEng = $StrCourse." JudgeGirl system";
 
 # Site Informations
-$TimeZone  = 'Asia/Taipei';
-$JudgeAddr = 'http://192.168.56.95/';
-$CourseAddr= 'http://192.168.56.95/';
-# $JudgeAddr = 'http://example.com/~user/';
-# $CourseAddr= 'http://example.com/course-address/';
+$TimeZone  = "Asia/Taipei";
+$JudgeAddr = "http://192.168.56.95/";
+$CourseAddr= "http://192.168.56.95/";
+# $JudgeAddr = "http://example.com/~user/";
+# $CourseAddr= "http://example.com/course-address/";
 
 # DB information, these tokens should not appear twice
 # or Bash script would fail
-$MySQLhost = 'localhost';
-$MySQLuser = '1031_programming';
-$MySQLpass = '0p;/9ol.8ik,';
-$MySQLdatabase = '1031_programming';
+$MySQLhost = "localhost";
+$MySQLuser = "1031_programming";
+$MySQLpass = "0p;/9ol.8ik,";
+$MySQLdatabase = "1031_programming";
 
 # Instructor Name
-$InstName = '林宗男';
+$InstName = "林宗男";
 
-# TA informations, should be separated by ', '
-$TAnames   = 'TA2 Two, TA1 One';
-$TAemails  = 'ta2@mail.com, ta1@mail.com';
+# TA informations, should be separated by ", "
+$TAnames   = "TA2 Two, TA1 One";
+$TAemails  = "ta2@mail.com, ta1@mail.com";
 
 # TODO: add this into install.pl
-$testPathBase = '/home/1031_programming/problems/upload';
-$webRoot = '/home/1031_programming/public_html';
-$probDescBaseUrl = 'docs';
+$testPathBase = "/home/1031_programming/problems/upload";
+$webRoot = "/home/1031_programming/public_html";
+$probDescBaseUrl = "docs";
 $probDescPath = "$webRoot/$probDescBaseUrl"; # path to problem descriptions
-$restartScript = '/home/1031_programming/judge/restart.sh';
+$restartScript = "/home/1031_programming/judge/restart.sh";
 ?>
 
 <?php
